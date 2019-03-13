778fd6dde943b3ae199048fca4c1a855361ade64
mariusweb <39734577+mariusweb@users.noreply.github.com>
2019-03-13 09:22:24 +0200

    Added more style

--- assets/scripts/custom.js
+++ assets/scripts/custom.js
@@ -1,3 +1,12 @@
+(function(){
+
 "use strict";
 
 console.log("JS Veikia!");
+
+let buttonLeft = document.getElementsByClassName("button-left");
+
+let menuNav = document.querySelector(".menu ul");
+console.log(menuNav);
+
+}());
